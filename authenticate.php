1cd24ca23716dab571d1581ee9abd86d5a86749e
Sam Marschke <samuel.marschke@my.jcu.edu.au>
2015-05-23 11:43:34 +1000

    Started Authentication

--- authenticate.php
+++ authenticate.php
@@ -1,6 +1,14 @@
 <?php
 include("dbconnect.php");
+session_start();
 
+if(isset($_POST['username'])) {
+    
+} else if (isset($_SESSION['account_type'])) {
+
+} else {
+    echo "Please Log in...";
+}
 
 
 ?>
